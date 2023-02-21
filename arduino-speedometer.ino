934b3f44f1e72bda4642f85ce8ca92173c6c0a17
ssoftin <125917871+ssoftin@users.noreply.github.com>
2023-02-21 10:29:13 +0000

    some comments are added

--- arduino-speedometer.ino
+++ arduino-speedometer.ino
@@ -1,14 +1,18 @@
+// these scary blocks of 1 and 0 are just monochrome images converted from image file to code format
+// I draw each symbol from 0 to 9 and corverted them to blocks of code
 
+// image `km/h`
 const uint8_t frame_kmh_40x16[] PROGMEM = {
 	0b00000000, 0b00000000, 0b00000000, 0b11111000, 0b11111000, 0b00000000, 0b10000000, 0b11000000, 0b01100000, 0b00100000, 0b00000000, 0b00000000, 0b11100000, 0b11100000, 0b11000000, 0b01100000, 0b11100000, 0b11000000, 0b11100000, 0b01100000, 0b11100000, 0b11000000, 0b00000000, 0b00000000, 0b00000000, 0b10000000, 0b11110000, 0b00111000, 0b00000000, 0b00000000, 0b11111000, 0b11111000, 0b11000000, 0b01100000, 0b01100000, 0b11100000, 0b11000000, 0b00000000, 0b00000000, 0b00000000, 
 	0b00000000, 0b00000000, 0b00000000, 0b00011111, 0b00011111, 0b00000011, 0b00000011, 0b00000110, 0b00001100, 0b00011000, 0b00000000, 0b00000000, 0b00011111, 0b00011111, 0b00000000, 0b00000000, 0b00011111, 0b00011111, 0b00000000, 0b00000000, 0b00011111, 0b00011111, 0b00000000, 0b00000000, 0b00011100, 0b00001111, 0b00000001, 0b00000000, 0b00000000, 0b00000000, 0b00011111, 0b00011111, 0b00000000, 0b00000000, 0b00000000, 0b00011111, 0b00011111, 0b00000000, 0b00000000, 0b00000000, 
 };
+// image `m/h`
 const uint8_t frame_mh_40x16[] PROGMEM = {
 	0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b11100000, 0b11100000, 0b11000000, 0b01100000, 0b11100000, 0b11000000, 0b11100000, 0b01100000, 0b11100000, 0b11000000, 0b00000000, 0b00000000, 0b00000000, 0b10000000, 0b11110000, 0b00111000, 0b00000000, 0b00000000, 0b11111000, 0b11111000, 0b11000000, 0b01100000, 0b01100000, 0b11100000, 0b11000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 
 	0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00011111, 0b00011111, 0b00000000, 0b00000000, 0b00011111, 0b00011111, 0b00000000, 0b00000000, 0b00011111, 0b00011111, 0b00000000, 0b00000000, 0b00011100, 0b00001111, 0b00000001, 0b00000000, 0b00000000, 0b00000000, 0b00011111, 0b00011111, 0b00000000, 0b00000000, 0b00000000, 0b00011111, 0b00011111, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 
 };
 
-
+// big images from 0 to 9
 const uint8_t frame0_40x56[] PROGMEM = {
 	0b00000000, 0b00000000, 0b00000000, 0b10000000, 0b01100000, 0b01110000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b11111000, 0b00111000, 0b11001000, 0b11110000, 0b11110000, 0b11100000, 0b11000000, 0b00000000, 0b00000000, 0b00000000, 
 	0b00000000, 0b00000000, 0b00000000, 0b11111111, 0b11111111, 0b11111111, 0b11111110, 0b11111110, 0b11111101, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000000, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b00000000, 0b00000000, 0b00000000, 
@@ -101,7 +105,6 @@ const uint8_t frame9_40x56[] PROGMEM = {
 };
 
 
-
 const uint8_t *frames[] = {
   frame0_40x56,
   frame1_40x56,
@@ -117,27 +120,31 @@ const uint8_t *frames[] = {
 
 
 
-
 #define PIN_SPEED 2   // speed pulse pin
 #define PIN_TYPE  3   // pin to choose m/h or km/h 
 #define Y_POSITION 8  // vertical position, center screen value: 4
 #define INTERRUPT_SPEED 0
 
 
+// graphics library
 #include <GyverOLED.h>
 GyverOLED<SSD1306_128x64, OLED_BUFFER> oled;
 
+// settings for averaging buffer
 #define SPEEDBUFSIZE  70
-#define SPEEDAVGINTERVAL 1000
+#define SPEEDAVGINTERVAL 1000 // 1000ms = 1sec
 
+// data struct for averaging buffer
 struct SPEEDMARK {
   float speed;
   unsigned long time;
 };
 
+// averaging buffer
 SPEEDMARK speedbuf[SPEEDBUFSIZE];
 volatile unsigned int speedhead = 0;
 
+// add new speed value to the averaging buffer
 void AddSpeed(float s) {
   speedbuf[speedhead].speed = s;
   speedbuf[speedhead].time = millis();
@@ -145,6 +152,7 @@ void AddSpeed(float s) {
   if (speedhead == SPEEDBUFSIZE) speedhead = 0;
 }
 
+// get average speed for SPEEDAVGINTERVAL period
 float GetAvgSpeed() {
   unsigned int strart = speedhead;
   unsigned int i = strart;
@@ -163,7 +171,9 @@ float GetAvgSpeed() {
   return sum;
 }
 
-
+// interrupt is fired each impuls of speed signal 10hz equals 30km/h
+// it calculates the current speed by counting time between impulses
+// and adds it to the averaging buffer
 void InterruptSpeed() {
   static unsigned long prev = 0;
   unsigned long cur = millis();
@@ -175,21 +185,22 @@ void InterruptSpeed() {
   prev = cur;
 }
 
-
+// converts speed to miles if requred and
+// print images of digits to the screen
 void PrintSpeed() {
   float miles = 1;
-  if (digitalRead(PIN_TYPE)) miles = 1.60934;
+  if (digitalRead(PIN_TYPE)) miles = 1.60934; // miles/kilometers coefficient
   unsigned int speed = GetAvgSpeed() / miles;
   speed = speed % 999;
   unsigned int d1, d2, d3;
-  d1 = speed / 100;
-  d2 = (speed % 100) / 10;
-  d3 = (speed % 10);
+  d1 = speed / 100;		// 100
+  d2 = (speed % 100) / 10;    	//  10
+  d3 = (speed % 10);		//   1
 
   oled.clear();
   if (d1 == 0) {
-    if (miles == 1) oled.drawBitmap(4, 36+Y_POSITION,  frame_kmh_40x16, 40, 16);
-    else            oled.drawBitmap(4, 36+Y_POSITION,  frame_mh_40x16, 40, 16);
+    if (miles == 1) oled.drawBitmap(4, 36+Y_POSITION,  frame_kmh_40x16, 40, 16); // kilometers
+    else            oled.drawBitmap(4, 36+Y_POSITION,  frame_mh_40x16, 40, 16);  // miles
   }
 
   if (d1)       oled.drawBitmap(4,  Y_POSITION, frames[d1], 40, 56);
@@ -198,7 +209,7 @@ void PrintSpeed() {
   oled.update();
 }
 
-
+// initialization - runs only ones
 void setup() {
   pinMode(PIN_TYPE, INPUT_PULLUP);
   pinMode(PIN_SPEED, INPUT_PULLUP);
@@ -215,7 +226,7 @@ void setup() {
   attachInterrupt(INTERRUPT_SPEED, InterruptSpeed, FALLING);
 }
 
-
+// main loop - just preent speed and wait 0.1sec
 void loop() {
   PrintSpeed();
   delay(100);
